f2055d9769c4ef0385c332c4ddde16c8996fefb0
Anton Tomashevskiy <anton.tomashevskiy@yandex.ru>
2025-11-26 23:31:00 +0300

    123

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
-import clsx from 'clsx';
+import { StrictMode, CSSProperties, useState } from 'react';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { defaultArticleState, OptionType } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -12,20 +11,38 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+type PageStyle = {
+	fontFamilyOption: OptionType;
+	fontSizeOption: OptionType;
+	fontColor: OptionType;
+	contentWidth: OptionType;
+	backgroundColor: OptionType;
+};
+
 const App = () => {
+	const [pageStyle, setPageStyle] = useState(defaultArticleState);
+
 	return (
 		<main
-			className={clsx(styles.main)}
+			className={styles.main}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': pageStyle.fontFamilyOption.value,
+					'--font-size': pageStyle.fontSizeOption.value,
+					'--font-color': pageStyle.fontColor.value,
+					'--container-width': pageStyle.contentWidth.value,
+					'--bg-color': pageStyle.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				onApply={(newStyle: PageStyle) => {
+					setPageStyle(newStyle);
+				}} // Применяет новые стили
+				onReset={() => {
+					setPageStyle(defaultArticleState);
+				}} // Сбрасывает стили
+			/>
+
 			<Article />
 		</main>
 	);
